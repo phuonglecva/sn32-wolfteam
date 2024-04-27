9c16f0bf41385670db60b65c4fab456effc8df87
Your Name <you@example.com>
2024-04-27 08:49:08 +0000

    update coordinator_sevice

--- coordinator_service.py
+++ coordinator_service.py
@@ -3,7 +3,7 @@ from typing import List
 import fastapi
 import json
 import aiohttp
-
+import numpy as np
 
 class ValidatorService:
     def __init__(self) -> None:
@@ -11,14 +11,18 @@ class ValidatorService:
         
         self.validator_hosts = {}
         
+        self.load_hosts()
+        
     def load_hosts(self):
         with open("validator_hosts.json", "r") as f:
             self.validator_hosts = json.load(f)
+            print(self.validator_hosts)
 
     
     async def get_distance(self, texts: List[str], validator: str):
         # 1 validator can map to multiple hosts
         hosts = self.validator_hosts.get(validator)
+        print(self.validator_hosts, hosts)
         if hosts is None:
             print(f"Validator {validator} not found")
             return None
@@ -26,7 +30,7 @@ class ValidatorService:
         async with aiohttp.ClientSession() as session:
             tasks = []
             for host in hosts:
-                url = f"http://{host}/texts/distances"
+                url = f"{host}/texts/distances"
                 data = {
                     "texts": texts,
                     "validator": validator
@@ -36,8 +40,10 @@ class ValidatorService:
             responses = await asyncio.gather(*tasks)
             results = []
             for response in responses:
-                results.append(await response.json())
-            return results
+                response = await response.json()
+                results.append(response['distances'])
+        results = np.array(results)
+        return results.mean(axis=0).tolist()
     
 app = fastapi.FastAPI()
 
@@ -57,3 +63,8 @@ class TextRequest(BaseModel):
 async def read_item(text_req: TextRequest):
     distance = await validator_service.get_distance(text_req.texts, text_req.validator)
     return {"distances": distance}
+
+
+if __name__=='__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=9000)
