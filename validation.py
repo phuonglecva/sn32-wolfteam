84a27513cb6ac7d4b6c6444f41e7fefe786131ba
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-09 11:22:50 +0700

    add log

--- validation.py
+++ validation.py
@@ -70,6 +70,7 @@ def infer_distance(texts):
         print(f'distance result: {distance_result}')
         time_end = time.time_ns()
         print(f'time processing distance: {(time_end - time_start) // 1000_000} ms')
+        distance_result = [not r for r in distance_result]
         print_accuracy(distance_result, 'distance_result')
 
         return distance_result
