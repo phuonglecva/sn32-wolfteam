b17fa10baf51882cf8906bab7f7cbe5c5681b1d4
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-17 22:25:19 +0700

    move file

--- validator_search.py
+++ validator_search.py
@@ -1,12 +1,15 @@
+import shutil
+
 from validation import infer_distance
 import json
 import os
 
-if __name__ == '__main__':
 
+def search_validator():
     input_dir = '/root/sample_data/'
     # files = os.listdir(input_dir)
-    directories = [os.path.join(input_dir, d) for d in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, d))]
+    directories = [os.path.join(input_dir, d) for d in os.listdir(input_dir) if
+                   os.path.isdir(os.path.join(input_dir, d))]
     for directory in directories:
         files = os.listdir(directory)
         for file in files:
@@ -19,3 +22,16 @@ if __name__ == '__main__':
                     validator_hotkey = data['validator_hotkey']
                     infer_distance(texts, validator_hotkey)
 
+
+def move_file():
+    directory = '/root/sample_data/'
+    files = [file for file in os.listdir(directory) if not os.path.isdir(os.path.join(directory, file))]
+    for file in files:
+        file_path = os.path.join(directory, file)
+        dir_name = file.split('_')[0]
+        target_dir = os.path.join(directory, dir_name)
+        shutil.move(file_path, target_dir)
+
+
+if __name__ == '__main__':
+    move_file()
