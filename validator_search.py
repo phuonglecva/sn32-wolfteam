46685c151bb5c72d61606c28ef6731e23e8489b3
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-18 21:38:55 +0700

    find file for /root/sample_data/5F4tQyWrhfGVcNhoqeiNsR6KjD4wMZ2kfhLj4oHYuyHbZAc3

--- validator_search.py
+++ validator_search.py
@@ -7,7 +7,7 @@ import os
 
 
 def search_validator():
-    input_dir = '/root/sample_data/5Fq5v71D4LX8Db1xsmRSy6udQThcZ8sFDqxQFwnUZ1BuqY5A'
+    input_dir = '/root/sample_data/5F4tQyWrhfGVcNhoqeiNsR6KjD4wMZ2kfhLj4oHYuyHbZAc3'
     # files = os.listdir(input_dir)
     directories = [os.path.join(input_dir, d) for d in os.listdir(input_dir) if
                    os.path.isdir(os.path.join(input_dir, d))]
