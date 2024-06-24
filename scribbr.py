e044928cc475f8c7513570e6d89a578fd4bfdda7
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-24 21:50:44 +0700

    update code

--- scribbr.py
+++ scribbr.py
@@ -3,7 +3,7 @@ import time
 
 import requests
 
-SAMPLE_DATA_DIR = '/Users/nannan/Downloads/Bittensor_storage/test_sample_data'
+SAMPLE_DATA_DIR = '/root/sample_sent_data_pile22'
 # API = "http://130.250.178.211:63443/predict"
 # API = "http://130.250.178.211:54337/predict"
 # API = "http://90.55.27.227:40175/predict"
@@ -12,7 +12,7 @@ API = 'http://8.12.5.23:34370/predict'
 
 def detect(text):
     url = 'https://www.scribbr.com/ai-detector.php'
-    apikey = 'c0e9686ba1f849950ce63fd12f0d4ae2fbbb4087'
+    apikey = '67cf2e217a04dc0352ce93f266adceab32aa2a36'
     params = {
         'url': url,
         'apikey': apikey,
@@ -41,7 +41,7 @@ def detect(text):
     })
     response = requests.post('https://api.zenrows.com/v1/', params=params, headers=headers, data=data, timeout=4)
     score = response.json()["data"]["text_score"]
-    # print(f'score = {score}')
+    print(f'score = {score}')
     return score > 0.7
 
 
@@ -76,19 +76,31 @@ def run_test():
         data = read_file(f_path)
         if data is None:
             continue
-        result = data['labels'][150:]
-        pred = call_api(data['texts'][150:])
-        if len(result) != len(pred):
-            print(f"len(result) = {len(result)}, len(pred) = {len(pred)}")
+
+        first = [False] * 150
+        second = [True] * 150
+        result = []
+        result.extend(first)
+        result.extend(second)
+
+        auged_texts = data['auged_texts']
+        pred_result = []
+        for i in range(10):
+            texts = auged_texts[30 * i:30*i + 30]
+            pred = call_api(texts)
+            pred_result.extend(pred)
+
+        if len(result) != len(pred_result):
+            print(f"len(result) = {len(result)}, len(pred_result) = {len(pred_result)}")
             continue
-        comp = [result[i] == pred[i] for i in range(len(result))]
-        total_text += len(pred)
+        comp = [result[i] == pred_result[i] for i in range(len(result))]
+        total_text += len(pred_result)
         total_pred_correct += comp.count(True)
         end_time = time.time_ns()
         time_processing = (end_time - start_time) // 1000_000
 
         print(
-            f'file {f_name}, pred wrong: {comp.count(False)}, pred correct: {comp.count(True)}, accuracy: {comp.count(True) / len(pred)}, average: {total_pred_correct / total_text}, time_processing = {time_processing}ms')
+            f'file {f_name}, pred wrong: {comp.count(False)}, pred correct: {comp.count(True)}, accuracy: {comp.count(True) / len(pred_result)}, average: {total_pred_correct / total_text}, time_processing = {time_processing}ms')
 
 
 def is_ai(text):
@@ -123,7 +135,7 @@ def is_ai(text):
         })
         response = requests.post('https://api.zenrows.com/v1/', params=params, headers=headers, data=data, timeout=10)
         score = response.json()["data"]["text_score"]
-        # print(f"score = {score}")
+        print(f"score = {score}")
         return score >= 0.7
     except Exception as e:
         print(f'generated an exception: {e}')
