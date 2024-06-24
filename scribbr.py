c0be7270c357e0dba31adb3faced26c27ab8fefc
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-24 22:50:15 +0700

    Call 300 texts

--- scribbr.py
+++ scribbr.py
@@ -88,8 +88,8 @@ def run_test():
 
         auged_texts = data['auged_texts']
         pred_result = []
-        for i in range(3):
-            texts = auged_texts[100 * i:100*i + 100]
+        for i in range(1):
+            texts = auged_texts[300 * i:300*i + 300]
             print(f"type of texts: {type(texts)}")
             pred = call_api(texts)
             pred_result.extend(pred)
